758dd6201119530c93059ca9ccbc1152b975dec0
sharukh <csgosaifi5@gmail.com>
2024-10-09 16:15:20 +0530

    small fix

--- app/api/webhooks/clerk/route.ts
+++ app/api/webhooks/clerk/route.ts
@@ -56,7 +56,7 @@ export async function POST(req: Request) {
   // Get the ID and type
   const { id } = evt.data;
   const eventType = evt.type;
-
+  console.log(`Webhook with and ID of ${id} and type of ${eventType}`)
   // CREATE
   if (eventType === "user.created") {
     const { id, email_addresses, image_url, first_name, last_name, username } = evt.data;
@@ -69,6 +69,7 @@ export async function POST(req: Request) {
       lastName: last_name,
       photo: image_url,
     };
+console.log(user);
 
     const newUser = await createUser(user);
 
